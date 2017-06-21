68e9e1214300610c483e26d27649d3737419d911
GAO Liangjie <gaoliangjie849@pingan.com.cn>
2017-06-21 18:13:42 +0800

    .

--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ import { render } from 'react-dom';
 import './style.css'
 
 class Hello extends React.Component {
-	render () {
-		return (<p> hello world !!! </p>)
-	}
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            // 显示当前时间
+            now: Date.now()
+        }
+    }
+    render() {
+        return (
+            <div>
+                <p>hello world {this.state.now}</p>
+            </div>
+        )
+    }
 }
+
+// class Hello extends React.Component {
+// 	render () {
+// 		return (<p> hello world !!! </p>)
+// 	}
+// }
 
 render(
   <Hello />,
